5f87829bcd30eda8ddb3b7d6963c3f412e9e53d5
wildmango <vstk633@gmail.com>
2022-05-05 12:38:43 +0200

    Add tests

--- Bot/js/main.js
+++ Bot/js/main.js
@@ -1,72 +1,67 @@
 console.log("Im alive")
 
-function displayAnswer() {
-  document.getElementById("Answer").innerHTML = answer()
-}
+// import {displayAnswer} from "Bot/js/functions/displayAnswer"
 
-const myHeaders = new Headers();
-myHeaders.append("apikey", "O1FqERv8pJgMgEm1EkNjvh69OntweE0R");
-
-const requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
+// displayAnswer();
 
 
-
-
-function getCourse() {
-  let data = document.querySelector('#Answer')
-  fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=CZK&base=EUR", requestOptions)
-    .then(response =>
-    {
-      return response.text().then(function (text){
-        data.innerHTML = "1 EUR = " + text.split(":")[6].replaceAll("}","")
-      })
-    })
-
-}
-
-
-
-
-
-
-
-
-
-function getTime() {
-  const time = Date.now()
-  return new Date(time)
-}
-
+// function displayAnswer() {
+//   document.getElementById("Answer").innerHTML = answer(getCommand())
+// }
+//
+// const myHeaders = new Headers();
+// myHeaders.append("apikey", "O1FqERv8pJgMgEm1EkNjvh69OntweE0R");
+//
+// const requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow',
+//   headers: myHeaders
+// };
+//
+//
+// function getCourse() {
+//   let data = document.querySelector('#Answer')
+//   fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=CZK&base=EUR", requestOptions)
+//     .then(response =>
+//     {
+//       return response.text().then(function (text){
+//         data.innerHTML = "1 EUR = " + text.split(":")[6].replaceAll("}","")
+//       })
+//     })
+//
+// }
+//
+// function getTime() {
+//   const time = Date.now()
+//   return new Date(time)
+// }
+//
 function getName() {
   return "bot"
 }
-
-function getCommand() {
-  const command = document.getElementById("input").value;
-  return command
-}
-
+//
+// function getCommand() {
+//   return document.getElementById("input").value.toLowerCase()
+// }
+//
 function getHelp() {
-  return "Command list: time \n name \n course \n help "
+  return "Command list: time name course help"
 }
-
-function answer() {
-  let command = getCommand();
-  switch (command) {
-    case "time": return getTime()
-    case "name": return getName()
-    case "course": return getCourse()
-    case "help": return getHelp()
-    default: return "Command dost not exist. Write help to show commands"
-  }
-
-
-}
-
+//
+// function answer(obj) {
+//   // let command = getCommand();
+//   switch (obj) {
+//     case "time": return getTime()
+//     case "name": return getName()
+//     case "course": return getCourse()
+//     case "help": return getHelp()
+//     default: return "Command dost not exist. Write help to show commands"
+//   }
+//
+//
+// }
+module.exports = getName;
+// module.exports = getTime;
 
 
 
